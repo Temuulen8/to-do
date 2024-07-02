5c9ee93aed8a5c88836c36a3c6cf9d741cc4c4ce
Temuulen8 <b.temuulen2005@gmail.com>
2024-07-02 12:29:35 +0800

    out

--- to_do.js
+++ to_do.js
@@ -4,13 +4,12 @@ const taskProgressList = document.getElementById("taskProgressList");
 const taskDoneList = document.getElementById("taskDoneList");
 const taskBlockedList = document.getElementById("taskBlockedList");
 const addTaskBtn = document.getElementById("addTaskBtn");
+const saveBtn = document.getElementById("save-btn");
+const taskInput = document.getElementById("task-input");
+const taskStatus = document.getElementById("status");
 
 // VARIABLES FOR TASK
 const tasks = [
-  {
-    name: "Task One",
-    status: "TODO",
-  },
   {
     name: "Task Two",
     status: "INPROGRESS",
@@ -21,7 +20,7 @@ const tasks = [
   },
 ];
 
-function draw() {
+function zurah() {
   taskTodoList.innerHTML = "";
   taskProgressList.innerHTML = "";
   taskDoneList.innerHTML = "";
@@ -29,18 +28,29 @@ function draw() {
 
   for (let i = 0; i < tasks.length; i++) {
     console.log("TASKS", tasks);
+    let bColor = "";
+    if (tasks[i].status === "TODO") {
+      bColor = "border-black";
+    } else if (tasks[i].status === "INPROGRESS") {
+      bColor = "border-warning";
+    } else if (tasks[i].status === "DONE") {
+      bColor = "border-success";
+    } else if (tasks[i].status === "BLOCKED") {
+      bColor = "border-danger";
+    }
+
     const newTaskCard = `
-    <div class="d-flex justify-content-between align-items-center border border-1 rounded p-2">
-    <span>${tasks[i].name}</span>
-    <div>
-        <button class="btn">
-        <i class="bi bi-pencil"></i>
-        </button>
-        <button class="btn">
-        <i class="bi bi-trash"></i>
-        </button>
-    </div>
-    </div>
+      <div class="d-flex justify-content-between align-items-center border ${bColor} border-1 rounded p-2">
+      <span>${tasks[i].name}</span>
+      <div>
+          <button class="btn" data-bs-toggle="modal" data-bs-target="#taskModal">
+          <i class="bi bi-pencil "></i>
+          </button>
+          <button class="btn" onclick="deleteTask(${i})">
+          <i class="bi bi-trash "></i>
+          </button>
+      </div>
+      </div>
  `;
 
     switch (tasks[i].status) {
@@ -67,14 +77,21 @@ function draw() {
   }
 }
 
-addTaskBtn.addEventListener("click", function () {
+saveBtn.addEventListener("click", function () {
   const newTask = {
-    name: "task four",
-    status: "TODO",
+    name: taskInput.value,
+    status: taskStatus.value,
   };
   tasks.push(newTask);
-  draw();
+  zurah();
   console.log("TASKS", tasks);
 });
 
-draw();
+zurah();
+
+const deleteTask = (taskIndex) => {
+  console.log(taskIndex);
+  tasks.splice(taskIndex, 1);
+  zurah();
+  console.log("Task deleted", taskIndex);
+};
